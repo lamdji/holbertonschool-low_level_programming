ac7c8f6fe9bbccb651add4660504defd87a5fbd7
lamdji <djitte.l@gmail.com>
2023-10-23 15:39:17 -0700

    function that returns the length of a string,Prototype: int _strlen(char *s)

--- pointers_arrays_strings/2-strlen.c
+++ pointers_arrays_strings/2-strlen.c
@@ -1,17 +1,19 @@
 #include "main.h"
 #include <stdio.h>
 /*
- *_strlen: return of length of  
+ *_strlen: return of length of
  *@s: string
  *return: length of i
  */
 int _strlen(char *s)
 {
 	int i = 0;
-	while (*s != '\0') 
+	while (*s != '\0')
+
 	{
 	i++;
 	s++;
 	}
+
 	return i;
 }
