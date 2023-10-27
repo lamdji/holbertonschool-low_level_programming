595c00ec88bbe0ee00dc4983d9fd5b5015efba91
lamdji <djitte.l@gmail.com>
2023-10-27 13:01:19 -0700

    copies the string pointed to by src, including the terminating null byte (\0), to the buffer pointed to by dest

--- pointers_arrays_strings/5-main.c
+++ pointers_arrays_strings/5-main.c
@@ -1,5 +1,6 @@
 #include "main.h"
 #include <stdio.h>
+
 /**
  *  * main - check the code
  *   *
@@ -7,10 +8,10 @@
  *     */
 int main(void)
 {
-	char *s = "hello, world";
-	char *f = "world";
-	char *t;
-	t = _strstr(s, f);
-	printf("%s\n", t);
-	return (0);
+	    char s[10] = "My School";
+
+	        printf("%s\n", s);
+		    rev_string(s);
+		        printf("%s\n", s);
+			    return (0);
 }
