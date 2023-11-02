b4856976b88f8fca3971f46f9b1258cc20d3c96c
lamdji <djitte.l@gmail.com>
2023-11-02 09:24:15 -0700

    function that returns the factorial of a given number,Prototype: int factorial(int n);, If n is lower than 0, the function should return -1 to indicate an error Factorial of 0 is 1

--- recursion/3-factorial.c
+++ recursion/3-factorial.c
@@ -1,6 +1,10 @@
 #include "main.h"
 #include <stdio.h>
-
+/**
+ * factorial - return factorial of a given number.
+ * @n: int type number
+ * Return: factorial of number
+ */
 int factorial(int n)
 {
 	if (n < 0)
